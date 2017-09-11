30c972ef11d487cfe80a876cb07b536f622fd202
DUONGTRUONG-986\Duong Truong <johnangel135@gmail.com>
2017-09-10 21:09:49 -0500

    no message

--- MPP/project/source/Supermarket Shopping App/src/mpp/ssa/presentation/Controller.java
+++ MPP/project/source/Supermarket Shopping App/src/mpp/ssa/presentation/Controller.java
@@ -1,4 +1,33 @@
-package mpp.ssa.presentation;
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class Controller {
+
+    @FXML
+    private Button SignUp;
+
+    @FXML
+    private Button Login;
+
+    @FXML
+    private Button Cart;
+
+    @FXML
+    void Login(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SignUp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Cart(ActionEvent event) {
+
+    }
+
 }
