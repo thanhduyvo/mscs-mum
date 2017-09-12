09bf7365b66229b7135d319f681e5a16f7342097
DUONGTRUONG-986\Duong Truong <johnangel135@gmail.com>
2017-09-12 14:28:48 -0500

    no message

--- MPP/project/source/Supermarket Shopping App/src/mpp/ssa/presentation/Controller.java
+++ MPP/project/source/Supermarket Shopping App/src/mpp/ssa/presentation/Controller.java
@@ -1,30 +1,101 @@
 package mpp.ssa.presentation;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
+import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 
-import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.*;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Controller {
+import mpp.ssa.domain.*;
+
+public class Controller{
     @FXML
-    private Button LoginBtn;
+    private Button LoginBtn, CheckLoginBtn, BackBtn;
+    @FXML
+    private TextField UsernameField;
+    @FXML
+    private PasswordField PasswordField;
+    @FXML
+    private ListView listView;
+
+    private final BorderPane root = Main.getRoot();
+    private final AnchorPane PaneListview = Main.getPanelistView();
+    private Scene prviousScene = Main.primaryStage.getScene();
+
+    public static String USERNAME, PASSWORD;
+    public static String getUsername(){
+        return USERNAME;
+    }
+    public static String getPassword(){
+        return PASSWORD;
+    }
+    public Controller() {
+
+    }
+
+    @FXML
+    public void initialize(){
+        Product a = new Product();
+        a.setProductName("item1");
+        a.setUnitCost(121);
+        ObservableList<HBox> items = FXCollections.observableArrayList();
+        items.add(showProduct(a));
+//        AnchorPane ListView = new AnchorPane();
+//        try {
+//            ListView = FXMLLoader.load(getClass().getResource("list_view.fxml"));
+//            Main.root.setCenter(ListView);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
+    }
+
 
     @FXML
     void handleLogin(ActionEvent event){
+        prviousScene = Main.primaryStage.getScene();
+        AnchorPane Login = new AnchorPane();
         try {
-            AnchorPane login = FXMLLoader.load(getClass().getResource("login_screen.fxml"));
-            BorderPane pane = Main.getRoot();
-            pane.setCenter(login);
+            Login = FXMLLoader.load(getClass().getResource("login_screen.fxml"));
+            Main.primaryStage.setScene(new Scene(Login,600,400));
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    void handleBackBtn(ActionEvent event){
+        Main.primaryStage.setScene(prviousScene);
+    }
+
+    @FXML
+    void handleCheckLogin(ActionEvent event){
+        USERNAME = UsernameField.getText().toString();
+        PASSWORD = PasswordField.getText().toString();
+    }
+
+
+
+
+    public HBox showProduct(Product product){
+        HBox hbox = new HBox();
+        Label name = new Label(product.getProductName() + "-----------");
+        Label cost = new Label("$" + String.valueOf(product.getUnitCost()));
+        hbox.getChildren().addAll(name,cost);
+        return hbox;
     }
 
 }
--- MPP/project/source/Supermarket Shopping App/src/mpp/ssa/presentation/Main.java
+++ MPP/project/source/Supermarket Shopping App/src/mpp/ssa/presentation/Main.java
@@ -8,25 +8,40 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
+    public static Stage primaryStage;
+
     public static BorderPane root = new BorderPane();
 
     public static BorderPane getRoot(){
         return root;
     }
 
+    public static AnchorPane PanelistView = new AnchorPane();
+
+    public static AnchorPane getPanelistView(){ return PanelistView;}
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        root = FXMLLoader.load(getClass().getResource("home_screen.fxml"));
-        primaryStage.setTitle("Shopping - MUM");
-        primaryStage.setScene(new Scene(root, 1042, 635));
-        primaryStage.show();
-
+    public void start(Stage primaryStage){
+        this.primaryStage = primaryStage;
+        try {
+            root = FXMLLoader.load(getClass().getResource("home_screen.fxml"));
+            primaryStage.setTitle("Shopping - MUM");
+            primaryStage.setScene(new Scene(root, 1042, 635));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+
 
     public static void main(String[] args) {
         launch(args);
