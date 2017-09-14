fd67d0d02fc63bc1134e31c2aa3079439d52de35
DUONGTRUONG-986\Duong Truong <johnangel135@gmail.com>
2017-09-14 13:33:48 -0500

    no message

--- MPP/project/source/Supermarket Shopping App/src/mpp/ssa/presentation/SignupController.java
+++ MPP/project/source/Supermarket Shopping App/src/mpp/ssa/presentation/SignupController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import mpp.ssa.bus.CustomerBUS;
+import mpp.ssa.domain.Customer;
 
 import java.io.IOException;
 
@@ -33,17 +35,40 @@ public class SignupController extends HomeController {
     @FXML
     private TextField txtEmail;
 
+    private Customer customer = new Customer();
+
 
     public void createSignupPane(){
         try {
             SignupPane = FXMLLoader.load(getClass().getResource("signup.fxml"));
+            setInfo();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public void setInfo(){
+        txtAddress = (TextField) SignupPane.lookup("#txtAddress");
+        txtCardNumber = (TextField) SignupPane.lookup("#txtCardNumber");
+        txtEmail = (TextField) SignupPane.lookup("#txtEmail");
+        txtName = (TextField) SignupPane.lookup("#txtName");
+        txtPhone = (TextField) SignupPane.lookup("#txtPhone");
+        txtShippingAddress = (TextField) SignupPane.lookup("#txtShippingAddress");
+    }
+
     @FXML
     public void handleConfirmBtn(){
+       saveCustomer();
+       CustomerBUS.getCustomerBUS().register(customer);
 
+    }
+
+    public void saveCustomer(){
+        customer.setAddress(txtAddress.getText());
+        customer.setBankCardNo(txtCardNumber.getText());
+        customer.setCustomerName(txtName.getText());
+        customer.setPassword(passPassword.getText());
+        customer.setEmail(txtEmail.getText());
+        customer.setShippingAddress(txtShippingAddress.getText());
     }
 }
